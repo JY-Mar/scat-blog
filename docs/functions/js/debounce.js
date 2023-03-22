b0e5e4311fd7fb2a4e2bad22f578f64bba63e5f1
Scientificat <51430248+JY-Mar@users.noreply.github.com>
2023-03-22 15:13:19 +0800

    Update debounce.js

--- docs/functions/js/debounce.js
+++ docs/functions/js/debounce.js
@@ -4,11 +4,12 @@ let timeout = null
  * 防抖原理：一定时间内，只有最后一次操作，再过wait毫秒后才执行函数
  * 立即执行：即多次触发事件，第一次会立即执行函数，之后在设定wait事件内触犯的事件无效，不会执行。
  * 非立即执行函数： 多次触发事件，只会在最后一次触发事件后等待设定的wait时间结束时执行一次。
- *
- * @param {Function} func 要执行的回调函数
- * @param {Number} wait 延时的时间
- * @param {Boolean} immediate 是否立即执行。即多次触发事件，第一次会立即执行函数，之后在设定wait事件内触犯的事件无效，不会执行。（非立即执行：多次触发事件，只会在最后一次触发事件后等待设定的wait时间结束时执行一次。）
- * @return null
+ * 
+ * @description  : 防抖
+ * @param         {Function} func 要执行的回调函数
+ * @param         {Number} wait 延时的时间
+ * @param         {Boolean} immediate 是否立即执行。即多次触发事件，第一次会立即执行函数，之后在设定wait事件内触犯的事件无效，不会执行。（非立即执行：多次触发事件，只会在最后一次触发事件后等待设定的wait时间结束时执行一次。）
+ * @return        {*}
  */
 const debounce = (func, wait = 500, immediate = false) => {
   // 如果timeout不为null, 清除定时器
