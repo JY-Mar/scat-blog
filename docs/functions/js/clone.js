7d3cc79ab8047616b5298efdfe4277dc4ceb0395
Scientificat <51430248+JY-Mar@users.noreply.github.com>
2023-03-28 15:31:04 +0800

    Update clone.js

--- docs/functions/js/clone.js
+++ docs/functions/js/clone.js
@@ -1,4 +1,20 @@
-import diff from './diff'
+/**
+ * @description  : 是数组
+ * @param         {*} arg
+ * @return        {*}
+ */
+const isArray = arg => {
+  return Object.prototype.toString.call(arg) === '[object Array]'
+}
+
+/**
+ * @description  : 是对象
+ * @param         {*} arg
+ * @return        {*}
+ */
+const isObject = arg => {
+  return Object.prototype.toString.call(arg) === '[object Object]'
+}
 
 /**
  * @description  : 递归复制属性值实现深拷贝
@@ -9,12 +25,14 @@ const clone = (obj) => {
   // 对常见的“非”值，直接返回原来值
   if ([null, undefined, NaN, false].includes(obj)) return obj
   // 原始类型直接返回
-  if (typeof obj !== 'object' && typeof obj !== 'function') return obj
+  if (!isObject(obj) || !isArray(obj) || typeof obj !== 'function') {
+    return obj
+  }
 
-  var o = diff.isArray(obj) ? [] : {}
+  var o = isArray(obj) ? [] : {}
   for (const i in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, i)) {
-      o[i] = typeof obj[i] === 'object' ? clone(obj[i]) : obj[i]
+      o[i] = isObject(obj) ? clone(obj[i]) : obj[i]
     }
   }
   return o
