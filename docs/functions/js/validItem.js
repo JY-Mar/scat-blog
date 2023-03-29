5003884827badcb6acab1124bc972c3e424aa4f3
Scientificat <51430248+JY-Mar@users.noreply.github.com>
2023-03-29 10:29:15 +0800

    Update validItem.js

--- docs/functions/js/validItem.js
+++ docs/functions/js/validItem.js
@@ -9,12 +9,15 @@ const validItem = value => {
   }
   if (typeof value === 'object') {
     if (value.constructor === Array) {
-      // 类型为数组，数组长度0
+      // 类型为数组，数组长度大于0
       return value.length > 0
-    }
-    if (Object.prototype.toString.call(value) === '[object Object]') {
-      // 对象属性数量0
+    } else if (Object.prototype.toString.call(value) === '[object Object]') {
+      // 类型为对象，对象属性数量大于0
       return Object.keys(value).length > 0
+    } else if (Object.prototype.toString.call(value) === '[object Date]') {
+      // 类型为日期，日期可被转换为longInt类型时间戳
+      const res = Date.parse(new Date(value))
+      return typeof res === 'number' && !isNaN(res)
     }
   }
   return true
